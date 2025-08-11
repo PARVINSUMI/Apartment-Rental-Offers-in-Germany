a256c63bcc5c1f9ddef061d492ef73c57a8b847f
PARVINSUMI <PARVIN091023@GMAIL.COM>
2025-08-11 23:03:43 +0200

    added sql file

---
+++ sql/SQL_analysis_german_apartment.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:65d708656aec5bbe2c355b901945280275ed7aa64b61e2f06c7d1471d311cf6a
+size 4417
---
+++ sql/insert_data-checkpoint.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0789841d2830158d684af093deee2d0f1e161d71951bfa8e88aad29ca4df2202
+size 167391757
